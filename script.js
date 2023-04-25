ac6a3d81c67c1483344c182520f33219eea4e494
hakonrh <hukonriisu@gmail.com>
2023-04-25 13:19:43 +0200

    Changed visuals for the nav menu when hovering Fixed a bug where you could win the game by doubleclicking the cards Background gradient now changes as you find pairs Congratulatory message is now a div instead of an alert Made card flipping animation work

--- script.js
+++ script.js
@@ -13,18 +13,31 @@ function flipCard() {
     const [card1, card2] = flippedCards;
 
     setTimeout(() => {
-      if (card1.classList[1] === card2.classList[1]) {
-        flippedCards.forEach((card) => (card.style.display = "none"));
-      } else {
+      if (flippedCards[0].classList[2] === flippedCards[1].classList[2]) {
         flippedCards.forEach((card) => card.classList.remove("is-flipped"));
+        flippedCards.length = 0;
+      } else {
+        if (card1.classList[1] === card2.classList[1]) {
+          flippedCards.forEach((card) => (card.style.display = "none"));
+        } else {
+          flippedCards.forEach((card) => card.classList.remove("is-flipped"));
+        }
+        flippedCards.length = 0;
       }
-      flippedCards.length = 0;
 
       const remainingCards = document.querySelectorAll(
         '.card:not([style="display: none;"])'
       );
+      if (remainingCards.length === 6) {
+        document.body.style.background =
+          "linear-gradient(180deg, rgb(205, 194, 172) 0%, rgba(0, 106, 171, 1) 77%)";
+      }
+      if (remainingCards.length === 2) {
+        document.body.style.background =
+          "linear-gradient(180deg, rgba(124,185,242,1) 0%, rgba(0,106,171,1) 77%)";
+      }
       if (!remainingCards.length) {
-        alert("Congrats! You found all the fish!");
+        document.querySelector("#congrats").style.display = "flex";
       }
     }, 1000);
   }
